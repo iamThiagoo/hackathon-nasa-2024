29d87f0e85ec21d8cd3d3821da8769f7d28a65f7
Thiago Ferreira <thiagofer2003@outlook.com>
2024-10-05 11:35:14 -0300

    feat: Scroll e zoom funcional

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
 // Criando a geometria do planeta (esfera)
 const geometry = new THREE.SphereGeometry(5, 32, 32);
-const material = new THREE.MeshBasicMaterial({color: 0x0077ff, wireframe: false});
+const material = new THREE.MeshBasicMaterial({color: 0x0077ff, wireframe: true});
 const planet = new THREE.Mesh(geometry, material);
 
-// Adiciona o planeta à cena
-scene.add(planet);
-camera.position.z = 20;
 
-// Função de renderização
-function animate() {
-    requestAnimationFrame(animate);
-    planet.rotation.y += 0.01; // Rotação do planeta
-    renderer.render(scene, camera);
-}
+import { Scene, WebGLRenderer, PerspectiveCamera } from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, w / h, 0.1, 100);
+const renderer = new WebGLRenderer({ antialias: true });
+const controls = new OrbitControls(camera, renderer.domElement);
+
+controls.minDistance = 10;
+controls.maxDistance = 60;
+camera.position.set(30 * Math.cos(Math.PI / 6), 30 * Math.sin(Math.PI / 6), 40);
+
+camera.position.z = 20;
+scene.add(planet);
+renderer.setSize(w, h);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+  renderer.setSize(w, h);
+  camera.aspect = w / h;
+  camera.updateProjectionMatrix();
+});
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  controls.update();
+  planet.rotation.y += 0.01;
+  renderer.render(scene, camera);
+};
+
 animate();
